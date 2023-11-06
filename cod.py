0c2bbac178671fe8ffcb01fd3e52c5c49d74f51f
Ronald <sincheleivaronald@gmail.com>
2023-11-06 18:02:23 -0500

    commit

---
+++ cod.py
@@ -0,0 +1,13 @@
+import os
+
+# Obtén la variable de entorno del usuario actual
+home = os.path.expanduser("~")
+
+# Ruta completa a la carpeta de descargas
+ruta_descargas = os.path.join(home, "Descargas")
+
+# Verifica si la carpeta de descargas existe
+if os.path.exists(ruta_descargas):
+    print(f"La carpeta de descargas se encuentra en: {ruta_descargas}")
+else:
+    print("La carpeta de descargas no se encontró en esta ubicación.")
